de1bccfc97a95de762e53269d6547ca6419a3612
François K <francoiska_fr@icloud.com>
2024-03-19 13:46:06 +0100

    Correct margin top mentions legales

--- src/views/MentionsView.vue
+++ src/views/MentionsView.vue
@@ -3,7 +3,7 @@
 <template>
   <div class="grid place-content-center">
     <div
-      class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20"
+      class="px-4 mt-16 sm:mt-8 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20"
     >
       <h2 class="max-w-lg mb-6 text-3xl sm:text-4xl sm:leading-none font-bold">
         Mentions légales
