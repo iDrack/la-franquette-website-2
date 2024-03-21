53a7f8f47452444830b82c44a724dfb69178f49a
François K <francoiska_fr@icloud.com>
2024-03-21 09:22:01 +0100

    Test deploiement via github Pages

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
+  base: '/la-franquette-website/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
